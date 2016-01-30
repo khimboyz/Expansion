bd887c0f4580de675dc91d9bd0cb0784e2d18cac
Khim Bermudez <khimboy10@gmail.com>
2016-01-30 03:06:17 -0800

    Basic stuff

--- src/main/java/me/khimboyz/expansion/Expansion.java
+++ src/main/java/me/khimboyz/expansion/Expansion.java
@@ -1,4 +1,14 @@
 package me.khimboyz.expansion;
 
-  public class Expansion {
+import org.bukkit.plugin.JavaPlugin;
+
+  public class Expansion extends JavaPlugin {
+    
+    public void onEnable() {
+      
+    }
+    
+    public void onDisable() {
+      
+    }
   }
